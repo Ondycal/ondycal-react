f4608bb32a7621466c02bff557fd28f81384ae91
faridzol <f.zolghadr@tapsell.ir>
2023-08-04 16:51:16 +0330

    chore: add primereact to project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
